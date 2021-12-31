3233cc4776b04e7914c1f2b59336c0caf8d15734
Oladejo Sunday <sundayoladejo13@gmail.com>
2021-12-31 03:04:38 +0100

    admin seeded and identity configured

---
+++ SocialMedia.API/Extensions/ServicesExtension.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace SocialMedia.API.Extensions
+{
+    public static class ServicesExtension
+    {
+        public static void AddDependencyInjection(this IServiceCollection services)
+        {
+
+            // Add Repository Injections Here
+            
+
+        }
+    }
+}
